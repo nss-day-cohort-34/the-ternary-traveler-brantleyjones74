02d7443cbd58a25e637cb9e5d1ed24a567299bb4
Brantley Jones <brantleyjones74@gmail.com>
2019-08-21 14:20:45 -0500

    fixed webpack and file structure

---
+++ src/scripts/main.js
@@ -0,0 +1,6 @@
+// imports function obj and gives obj variable name events
+import events from "./contact-form.js";
+
+// invoking of both functions inside the object named events
+events.contactsPostToDom();
+events.saveEvent();
